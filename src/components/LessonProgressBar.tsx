fb14e62f2d2a92ea21a0cfa05535781ebe3af955
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-01-10 13:32:12 -0500

    Switch progress bar variable path.

--- src/components/LessonProgressBar.tsx
+++ src/components/LessonProgressBar.tsx
@@ -1,6 +1,7 @@
 import { LessonProgressBarProps } from "../types/types";
+import { max } from "../data/game_settings";
 
-const LessonProgressBar = ({numAns, max}:LessonProgressBarProps) => {
+const LessonProgressBar = ({numAns}:LessonProgressBarProps) => {
   return (
     <section id="progress-box">
       <img id="progress-bar-fill" width={`${(numAns + 1) / (max / 100)}%`}></img>
